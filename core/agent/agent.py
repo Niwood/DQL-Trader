d4fba8e480fffde9ca3c87cf3ae659eb07c8917d
Robin <robinniwood@gmail.com>
2021-05-10 12:23:36 +0200

    fix for tf config

--- core/agent/agent.py
+++ core/agent/agent.py
@@ -28,9 +28,11 @@ import math
 from pathlib import Path
 
 import tensorflow as tf
-physical_devices = tf.config.list_physical_devices('GPU') 
-tf.config.experimental.set_memory_growth(physical_devices[0], True)
-
+try:
+    physical_devices = tf.config.list_physical_devices('GPU') 
+    tf.config.experimental.set_memory_growth(physical_devices[0], True)
+except:
+    pass
 
 
 class Agent:
